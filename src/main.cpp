c31b4683d78a57647e4e387fa301ec8f8133dea5
Albert <statealbert@gmail.com>
2023-03-08 10:40:47 -0500

    test non fonctionnel des ervo moteurs

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,25 @@
 #include <padNumerique.h> 
 
 
+uint8_t servoPin =1;
+
 
 const char* ssid = "Albert";
 const char* password = "state123";
+
+void servo_up(void){
+
+  for(int i=0; i<2000; i++){
+    digitalWrite(servoPin, HIGH);
+    delayMicroseconds(900);
+    digitalWrite(servoPin, LOW);
+    delayMicroseconds(20000);
+  }
+
+
+
+  
+}
 
 
 void setup() {
@@ -15,15 +31,17 @@ void setup() {
 
   Serial.printf("Hello World!");
   
-  WiFi.begin(ssid, password);
-    Serial.println("\nConnecting");
+  // WiFi.begin(ssid, password);
+  //   Serial.println("\nConnecting");
 
-    while(WiFi.status() != WL_CONNECTED){
-        Serial.print(".");
-        delay(100);
-    }
+  //   while(WiFi.status() != WL_CONNECTED){
+  //       Serial.print(".");
+  //       delay(100);
+  //   }
 
-    Serial.println("\nConnected to the WiFi network");
+  //   Serial.println("\nConnected to the WiFi network");
+  servo_up();
+  
 
     
 }
@@ -31,5 +49,8 @@ void setup() {
 void loop() {
   Serial.print(keypad.getKey());
   delay(250);
+  servo_up();
+
+  
 }
 
